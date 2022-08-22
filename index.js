4a597b2f3f3f5b8e5444eb590cc085b31d16c341
Saveliy <bizon1997mail@mail.ru>
2022-08-22 16:00:02 +0300

    init project

---
+++ index.js
@@ -0,0 +1,10 @@
+//git init
+//ls -a
+//git clone
+//git remote -v
+//git remote add origin https://github.com/1PlayerKillerGames/test-git-2.git'
+
+//git config
+//git config user.name "new name"
+//git config user.email ""
+//git status
